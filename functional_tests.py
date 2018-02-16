327e455770c90c6204998d67d3e71edf16d657f2
Kalu Kalu <kalukalu@Kalus-MacBook-Pro.local>
2018-02-17 07:54:16 +0800

    added tests taht fails when checking for second item

--- functional_tests.py
+++ functional_tests.py
@@ -39,19 +39,31 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 
 		table = self.browser.find_element_by_id("id_list_table")
-		self.asserTrue(
-			any(row.text == "1 :Buy peacock feathers" for row in rows)
-		)
+		rows = table.find_elements_by_tag_name("tr")
 
 		# There is still a text box inviting her to add another item. She
-		self.fail("Finish The Test!")
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+		inputbox = self.browser.find_element_by_id("id_new_item")
+		inputbox.send_keys("Use peacock feathers to make a fly")
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id("id_list_table")
+		rows = table.find_elements_by_tag_name("tr")
+		self.assertIn('1: Buy peacock feathers', [row.text for row in 
+			rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly',
+			[row.text for row in rows]
+		)
 
 		# The page updates again, and now shows both items on her list
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
+
+		self.fail("Finish The Test!")
 
 		# She visits that URL - her to-do list is still there.
 
